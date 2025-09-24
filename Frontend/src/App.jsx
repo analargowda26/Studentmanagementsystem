feedc790b2419d7663282f8474750588d623def3
analargowda26 <analargowda26@gmail.com>
2025-09-24 18:59:42 +0530

    added css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,38 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [students] = useState([
-    { id: 1, name: 'Thanush', age: 21 },
-    { id: 2, name: 'Pragna', age: 20 },
-    { id: 3, name: 'Swaroopa', age: 17 },
-    { id: 4, name: 'HariShankar', age: 22 }
-  ]);
+  const students = 
+  [
+    {
+      id:1,
+      name:"Anala",
+      age: 22,
+    },
+     {
+      id:2,
+      name:"Thanush",
+      age: 22,
+    },
+     {
+      id:3,
+      name:"Sneha",
+      age: 22,
+    }
 
+  ]
   return (
-    <div className="App">
-      <h1>Student Table</h1>
-      <div className='table'> 
-      <table border="1" cellPadding='8' >
-        <thead>
-          <tr>
+  <div className = "app-container">
+  <div className="card">
+    <div className="header">
+      <h1>Student List</h1>
+      <button className="create-student">Create Student</button>
+      </div>
+
+      <div className="table">
+        <table>
+          <thead>
+             <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Age</th>
           </tr>
-        </thead>
-        <tbody>
-          {students.map((student) => (
-            <tr key={student.id}>
-              <td>{student.id}</td>
-              <td>{student.name}</td>
-              <td>{student.age}</td>
+          </thead>
+          <tbody>
+            {students.map((student)=>(
+            <tr key= {student.id}>
+              <td> {student.id}</td>
+              <td> {student.name}</td>
+              <td> {student.age}</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  )
+  </div>
+  );
 }
 
 export default App
